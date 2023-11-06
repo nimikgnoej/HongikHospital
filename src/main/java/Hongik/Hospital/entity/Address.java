fb1d97c333be15cf6d4b22ecff5788d21db6bee2
JEONG KI MIN <97235034+nimikgnoej@users.noreply.github.com>
2023-11-06 17:18:40 +0900

    address의 생성자 추가

--- src/main/java/Hongik/Hospital/entity/Address.java
+++ src/main/java/Hongik/Hospital/entity/Address.java
@@ -8,7 +8,16 @@ import javax.persistence.Embeddable;
 @Embeddable
 @Getter @Setter
 public class Address {
+    public Address(String city,String street, String zipcode){
+        this.city = city;
+        this.street = street;
+        this.zipcode = zipcode;
+    }
     private String city;
     private String street;
     private String zipcode;
+
+    public Address() {
+
+    }
 }
